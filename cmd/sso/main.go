2219eb6445b7b92aa47964e0ed6bdc19c9877143
akolesnov <a.kolesnov@yandex.ru>
2026-02-28 18:27:34 +0300

    Добавлено логирование

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,16 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 	"sso/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
 
-	// TODO: иниацилизировать логгер
+	log.Info("starting application", slog.Any("cfg", cfg))
 
 	// TODO: инициализировать приложение (app)
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
